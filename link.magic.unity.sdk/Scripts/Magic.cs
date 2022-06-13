7882289ba377be6bc1450b64bdbcc83f1fd17a07
ethella <jerry@magic.link>
2022-06-13 16:34:44 -0700

    Add new instantiation method

--- link.magic.unity.sdk/Scripts/Magic.cs
+++ link.magic.unity.sdk/Scripts/Magic.cs
@@ -15,10 +15,20 @@ namespace link.magic.unity.sdk
         public readonly UserModule User;
 
         //Constructor
-        public Magic(string apikey)
+        public Magic(string apikey, EthNetwork network = EthNetwork.Mainnet, string locale = "en-US")
         {
-            var config = new EthNetworkConfiguration(EthNetwork.Mainnet);
-            var urlBuilder = new UrlBuilder(apikey, config);
+            var config = new EthNetworkConfiguration(network);
+            var urlBuilder = new UrlBuilder(apikey, config, locale);
+            UrlBuilder.Instance = urlBuilder;
+
+            Provider = new RpcProvider(urlBuilder);
+            User = new UserModule(Provider);
+            Auth = new AuthModule(Provider);
+        }
+
+        public Magic(string apikey, CustomNodeConfiguration config, string locale = "en-US")
+        {
+            var urlBuilder = new UrlBuilder(apikey, config, locale);
             UrlBuilder.Instance = urlBuilder;
 
             Provider = new RpcProvider(urlBuilder);
